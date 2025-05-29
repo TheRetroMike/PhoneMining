6d37cb325d305df92bb6766a84ec48c7e0196198
RetroMike <michaeldlesk@gmail.com>
2025-05-29 18:03:21 -0500

    Update termux_vrsc_advc_dual.sh

--- termux_vrsc_advc_dual.sh
+++ termux_vrsc_advc_dual.sh
@@ -25,11 +25,9 @@ echo "split" >> dualmine.config
 echo "focus" >> dualmine.config
 echo "screen 2 advc_miner/cpuminer -a YespowerAdvc -o $6 -u $7 -p $8 -t $9 --cpu-affinity ${10}" >> dualmine.config
 
-echo "screen -c dualmine.config" > startup.sh
-#echo "screen -r miner" >> startup.sh
-#echo "screen -dmS vrsc_miner ~/ccminer/ccminer -a verus -o $1 -u $2 -p $3 -t $4 --cpu-affinity $5" > startup.sh
-#echo "screen -dmS advc_miner ~/advc_miner/cpuminer -a YespowerAdvc -o $6 -u $7 -p $8 -t $9 --cpu-affinity ${10}" >> startup.sh
+echo "screen -s \"/bin/bash\" -c ~/dualmine.config -dmS miner" > startup.sh
+echo "screen -c dualmine.config" > ui-startup.sh
 chmod +x startup.sh
 mkdir -p ~/.termux/boot
 wget https://raw.githubusercontent.com/TheRetroMike/VerusCliMining/refs/heads/main/termux_boot_startup.sh -O ~/.termux/boot/termux_boot_startup.sh
-~/startup.sh
+~/ui-startup.sh
