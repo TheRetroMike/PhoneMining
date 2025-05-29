6fad8a26254dbff4d7c583f7edd7aa018b666362
RetroMike <michaeldlesk@gmail.com>
2025-05-29 17:00:12 -0500

    Update termux_vrsc_advc_dual.sh

--- termux_vrsc_advc_dual.sh
+++ termux_vrsc_advc_dual.sh
@@ -20,12 +20,12 @@ cd advc_miner
 
 #CONFIG
 cd ~
-echo "screen -dmS miner" > startup.sh
-echo "screen -S miner -X split" >> startup.sh
-echo "screen -S miner -X select 0" >> startup.sh
-echo "screen -S miner -X stuff \"~/ccminer/ccminer -a verus -o $1 -u $2 -p $3 -t $4 --cpu-affinity $5\"" >> startup.sh
-echo "screen -S miner -X select 1" >> startup.sh
-echo "screen -S miner -X stuff \"~/advc_miner/cpuminer -a YespowerAdvc -o $6 -u $7 -p $8 -t $9 --cpu-affinity ${10}\"" >> startup.sh
+echo "screen 1 ccminer/ccminer -a verus -o $1 -u $2 -p $3 -t $4 --cpu-affinity $5" > dualmine.config
+echo "split" >> dualmine.config
+echo "focus" >> dualmine.config
+echo "screen 2 advc_miner/cpuminer -a YespowerAdvc -o $6 -u $7 -p $8 -t $9 --cpu-affinity ${10}" >> dualmine.config
+
+echo "screen -dmS -c dualmine.config" > startup.sh
 echo "screen -r miner" >> startup.sh
 #echo "screen -dmS vrsc_miner ~/ccminer/ccminer -a verus -o $1 -u $2 -p $3 -t $4 --cpu-affinity $5" > startup.sh
 #echo "screen -dmS advc_miner ~/advc_miner/cpuminer -a YespowerAdvc -o $6 -u $7 -p $8 -t $9 --cpu-affinity ${10}" >> startup.sh
@@ -33,9 +33,3 @@ chmod +x startup.sh
 mkdir -p ~/.termux/boot
 wget https://raw.githubusercontent.com/TheRetroMike/VerusCliMining/refs/heads/main/termux_boot_startup.sh -O ~/.termux/boot/termux_boot_startup.sh
 ~/startup.sh
-echo "------------------------------"
-echo "Miner is installed and running. Update config using nano startup.sh"
-echo "To view Verus miner now or after restart, use screen -r vrsc_miner"
-echo "To view AdventureCoin miner now or after restart, use screen -r advc_miner"
-echo "------------------------------"
-screen -r miner
