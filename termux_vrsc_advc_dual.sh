d05fd1e28d9bec1127952285e684d8b351911bec
RetroMike <michaeldlesk@gmail.com>
2025-05-29 15:48:36 -0500

    Update termux_vrsc_advc_dual.sh

--- termux_vrsc_advc_dual.sh
+++ termux_vrsc_advc_dual.sh
@@ -20,10 +20,15 @@ cd advc_miner
 
 #CONFIG
 cd ~
-screen -L -Logfile miner.log
-echo "screen -dmS -L -Logfile miner.log vrsc_miner ~/ccminer/ccminer -a verus -o $1 -u $2 -p $3 -t $4 --cpu-affinity $5" > startup.sh
-echo "screen -dmS -L -Logfile miner.log advc_miner ~/advc_miner/cpuminer -a YespowerAdvc -o $6 -u $7 -p $8 -t $9 --cpu-affinity ${10}" >> startup.sh
-echo "screen -dmS miner tail -f ~/miner.log" >> startup.sh
+echo "screen -dmS miner" > startup.sh
+echo "screen -S miner -X split" >> startup.sh
+echo "screen -S miner -X select 0" >> startup.sh
+echo "screen -S miner -X stuff \"~/ccminer/ccminer -a verus -o $1 -u $2 -p $3 -t $4 --cpu-affinity $5\"" >> startup.sh
+echo "screen -S miner -X select 1" >> startup.sh
+echo "screen -S miner -X stuff \"~/advc_miner/cpuminer -a YespowerAdvc -o $6 -u $7 -p $8 -t $9 --cpu-affinity ${10}\"" >> startup.sh
+echo "screen -r miner" >> startup.sh
+#echo "screen -dmS vrsc_miner ~/ccminer/ccminer -a verus -o $1 -u $2 -p $3 -t $4 --cpu-affinity $5" > startup.sh
+#echo "screen -dmS advc_miner ~/advc_miner/cpuminer -a YespowerAdvc -o $6 -u $7 -p $8 -t $9 --cpu-affinity ${10}" >> startup.sh
 chmod +x startup.sh
 mkdir -p ~/.termux/boot
 wget https://raw.githubusercontent.com/TheRetroMike/VerusCliMining/refs/heads/main/termux_boot_startup.sh -O ~/.termux/boot/termux_boot_startup.sh
